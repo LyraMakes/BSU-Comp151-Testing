33cd4ef3e65b00e35f90cd170f0afe19efa94bc4
LyraMakes <blyra05@gmail.com>
2021-12-13 11:22:59 -0500

    Unshelving locals from BSU pc

---
+++ Assignment6.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+class Student:
+    def __init__(self, name, credits, grades):
+        
+
+
+
+
+def main():
+    """Program Entry Point"""
+    pass
+
+
+if __name__ == "__main__":
+    main()
